737878b3e39ea88565d268173551c553c7ec8673
runner <runner@pkrvmq0rgcvqdmg>
2025-07-23 01:55:58 +0000

    update drawio.sh:v28.0.6,firefox.sh:141.0,etcd.sh:v3.6.3

--- drawio.sh
+++ drawio.sh
@@ -2,7 +2,7 @@
 
 SHELL_FOLDER=$(cd $(dirname ${BASH_SOURCE[0]});pwd)
 
-VERSION="v28.0.4"
+VERSION="v28.0.6"
 
 if [ -n "$1" ];then VERSION=$1; fi
 
--- etcd.sh
+++ etcd.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-VERSION="v3.6.2"
+VERSION="v3.6.3"
 
 if [ -n "$1" ];then
     VERSION=$1
