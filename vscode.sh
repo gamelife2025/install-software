3bd44a1b8193190ce77a80799e28b1044196e14d
runner <runner@runnervm3ublj>
2025-09-26 01:36:24 +0000

    update hugo.sh:v0.150.1,vscode.sh:e3a5a

--- vscode.sh
+++ vscode.sh
@@ -2,7 +2,7 @@
 
 SHELL_FOLDER=$(cd $(dirname ${BASH_SOURCE[0]});pwd)
 
-fileUrl="https://vscode.download.prss.microsoft.com/dbazure/download/stable/0f0d87fa9e96c856c5212fc86db137ac0d783365/code-stable-x64-1758154038.tar.gz"
+fileUrl="https://vscode.download.prss.microsoft.com/dbazure/download/stable/e3a5acfb517a443235981655413d566533107e92/code-stable-x64-1758714233.tar.gz"
 fileName=`echo $fileUrl|awk -F "/" '{print $NF}'`
 
 _main() {
