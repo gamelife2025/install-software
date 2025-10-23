88806ccdf9349caa37d09c00f4a106fe9a1480eb
runner <runner@runnervmwhb2z>
2025-10-23 01:39:40 +0000

    update docker-compose.sh:v2.40.2,drawio.sh:v28.2.7,hugo.sh:v0.152.1

--- drawio.sh
+++ drawio.sh
@@ -2,7 +2,7 @@
 
 SHELL_FOLDER=$(cd $(dirname ${BASH_SOURCE[0]});pwd)
 
-VERSION="v28.2.5"
+VERSION="v28.2.7"
 
 if [ -n "$1" ];then VERSION=$1; fi
 
--- hugo.sh
+++ hugo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="v0.152.0"
+VERSION="v0.152.1"
 
 if [ -n "$1" ];then
     VERSION=$1
