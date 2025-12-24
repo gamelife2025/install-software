79b7a48745de50da312cfb44eddda4503c139ab8
runner <runner@runnervmh13bl>
2025-12-24 01:49:03 +0000

    update hugo.sh:v0.153.2,lua.sh:v5.5.0

--- hugo.sh
+++ hugo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="v0.153.1"
+VERSION="v0.153.2"
 
 if [ -n "$1" ];then
     VERSION=$1
--- lua.sh
+++ lua.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="v5.4.8"
+VERSION="v5.5.0"
 
 if [ -n "$1" ];then
     VERSION=$1
