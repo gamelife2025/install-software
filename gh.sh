5afe8ab69b1538a45b0ded076f7cad0e429994f0
runner <runner@runnervmjduv7>
2026-02-19 02:26:28 +0000

    update gh.sh:v2.87.0,hugo.sh:v0.156.0

--- gh.sh
+++ gh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="v2.86.0"
+VERSION="v2.87.0"
 
 if [ -n "$1" ];then
     VERSION=$1
--- hugo.sh
+++ hugo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="v0.155.3"
+VERSION="v0.156.0"
 
 if [ -n "$1" ];then
     VERSION=$1
