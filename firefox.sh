7c56947d0963f16df1824d0c1ffa6bcef1a2e58c
runner <runner@runnervmwffz4>
2026-02-25 02:25:23 +0000

    update docker-compose.sh:v5.1.0,firefox.sh:148.0

--- firefox.sh
+++ firefox.sh
@@ -2,7 +2,7 @@
 
 SHELL_FOLDER=$(cd $(dirname ${BASH_SOURCE[0]});pwd)
 
-fileUrl="https://download-installer.cdn.mozilla.net/pub/firefox/releases/147.0.4/linux-x86_64/zh-CN/firefox-147.0.4.tar.xz"
+fileUrl="https://download-installer.cdn.mozilla.net/pub/firefox/releases/148.0/linux-x86_64/zh-CN/firefox-148.0.tar.xz"
 fileName="Firefox-latest-x86_64.tar.bz2"
 
 _main() {
