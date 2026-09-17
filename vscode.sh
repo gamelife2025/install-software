e72fea87b7bfa30c0ca860037e76bc06d58a0ded
runner <runner@runnervmlun5p>
2026-09-17 03:56:25 +0000

    update vscode.sh:7debc

--- vscode.sh
+++ vscode.sh
@@ -2,7 +2,7 @@
 
 SHELL_FOLDER=$(cd $(dirname ${BASH_SOURCE[0]});pwd)
 
-fileUrl="https://vscode.download.prss.microsoft.com/dbazure/download/stable/645f29cc3176500b4b5762ba887cf2a7f0ffdf2c/code-stable-x64-1788901890.tar.gz"
+fileUrl="https://vscode.download.prss.microsoft.com/dbazure/download/stable/7debcd0e2acdea1c52de81bf9ee1620444407dda/code-stable-x64-1789458585.tar.gz"
 fileName=`echo $fileUrl|awk -F "/" '{print $NF}'`
 
 _main() {
